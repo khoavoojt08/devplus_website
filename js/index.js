84fc70230dd07970e0575df107cfb68c987a374b
minhnguyen <minhdacamst@gmail.com>
2024-03-15 08:25:31 +0700

    Feat/dp11 ngocminh testimonial (#16)
    * feat: dp11-testimonial
    
    * feat: dp11-testimonial
    
    * refactor: dp11-testimonial remove unused file
    
    * feat: dp11-testimonial
    
    * feat: dp11-testimonial
    
    * refactor: dp11-testimonial remove unused file
    
    * refactor: dp11-testimonial

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper-course', {
+const swiperCourse = new Swiper('.swiper-course', {
     autoplay: {
         delay: 3000,
         disableOnInteraction: false,
@@ -48,4 +48,15 @@ const swiper = new Swiper('.swiper-course', {
 
 
     }
+});
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
 });
